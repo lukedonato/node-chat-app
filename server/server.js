ad2715e6c75bca8014aae2a638d7c59ef5c4d65b
Luke Donato <luke.donato@tivix.com>
2017-05-12 11:51:17 -0400

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -13,12 +13,28 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
   socket.on('createMessage', (newMsg) => {
     io.emit('newMessage', {
       from: newMsg.from,
       text: newMsg.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: newMsg.from,
+    //   text: newMsg.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
