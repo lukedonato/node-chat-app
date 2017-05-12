9d7e995fec1a60934588b8e8161a6b42563b135c
Luke Donato <luke.donato@tivix.com>
2017-05-12 14:07:42 -0400

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -5,10 +5,25 @@ socket.on('connect', function () {
 
   socket.on('newMessage', function (newMsg) {
     console.log('New message', newMsg);
+    let li = $('<li></li>');
+    li.text(`${newMsg.from}: ${newMsg.text}`);
+
+    $('#messages').append(li);
   });
 
   socket.on('disconnect', () => {
     console.log('disconnected from server');
   });
 
+});
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function (){
+
+  });
 });
--- server/server.js
+++ server/server.js
@@ -18,8 +18,10 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (newMsg) => {
+  socket.on('createMessage', (newMsg, callback) => {
+    console.log('createMessage', newMsg);
     io.emit('newMessage', generateMessage(newMsg.from, newMsg.text));
+    callback('This is from the server.');
     // socket.broadcast.emit('newMessage', {
     //   from: newMsg.from,
     //   text: newMsg.text,
